a79dbcbdd04406cb441550bdcd964ee0e0f502fe
liar017 <142229265+liar017@users.noreply.github.com>
2023-08-18 22:04:37 +0800

    123

--- anemo/2018A1.py
+++ anemo/2018A1.py
@@ -21,7 +21,7 @@ h = 0.01
 M = int(15.2 / h)  + 1
 N = 5401
 
-x0 = 0; xL = 15.2; dx = h
+x0 = 0; xL = 15.2 * 0.001; dx = h * 0.001
 t0 = 0; tF = 5400; dt = 1
 
 k1 = 0.082;  k2 = 0.37;  k3 = 0.045;  k4 = 0.028
@@ -42,25 +42,34 @@ xspan = np.linspace(x0, xL, M)
 tspan = np.linspace(t0, tF, N)
 
 main_diag1 = (1 + 2 * r1) * np.ones(59)
-main_diag2 = (1 + 2 * r2) * np.ones(600)
-main_diag3 = (1 + 2 * r3) * np.ones(360)
-main_diag4 = (1 + 2 * r4) * np.ones(500)
+m1 = (k1 + k2) / dx * np.ones(1)
+main_diag2 = (1 + 2 * r2) * np.ones(599)
+m2 = (k2 + k3) / dx * np.ones(1)
+main_diag3 = (1 + 2 * r3) * np.ones(359)
+m3 = (k3 + k4) / dx * np.ones(1)
+main_diag4 = (1 + 2 * r4) * np.ones(499)
 
-main_diag = np.concatenate((main_diag1,main_diag2,main_diag3,main_diag4), axis = 0)
+main_diag = np.concatenate((main_diag1, m1, main_diag2, m2, main_diag3, m3, main_diag4), axis = 0)
 
 off_p1 = -r1 * np.ones(59)
-off_p2 = -r2 * np.ones(600)
-off_p3 = -r3 * np.ones(360)
-off_p4 = -r4 * np.ones(499)
+op1 = - k2 / dx * np.ones(1)
+off_p2 = -r2 * np.ones(599)
+op2 = - k3 / dx * np.ones(1)
+off_p3 = -r3 * np.ones(359)
+op3 = - k4 / dx * np.ones(1)
+off_p4 = -r4 * np.ones(498)
 
-off_diagp = np.concatenate((off_p1,off_p2,off_p3,off_p4),axis = 0)
+off_diagp = np.concatenate((off_p1, op1, off_p2, op2, off_p3, op3, off_p4),axis = 0)
 
 off_n1 = -r1 * np.ones(58)
-off_n2 = -r2 * np.ones(600)
-off_n3 = -r3 * np.ones(360)
-off_n4 = -r4 * np.ones(500)
+on1 = - k1 / dx * np.ones(1)
+off_n2 = -r2 * np.ones(599)
+on2 = - k2 / dx * np.ones(1)
+off_n3 = -r3 * np.ones(359)
+on3 = - k3 / dx * np.ones(1)
+off_n4 = -r4 * np.ones(499)
 
-off_diagn = np.concatenate((off_n1,off_n2,off_n3,off_n4),axis = 0)
+off_diagn = np.concatenate((off_n1, on1, off_n2, on2, off_n3, on3, off_n4),axis = 0)
 
 diagonals = [main_diag, off_diagp, off_diagn]
 
@@ -92,7 +101,11 @@ for k in range(1, N):
     c = np.zeros(M-4)
     b1 = np.array([r1*T[0, k], r4*T[-1, k]])
     b1 = np.insert(b1, 1, c)
-    b2 = T[1:M-1, k-1]
+    b21 = T[1:60, k-1]
+    b22 = T[61: 660, k-1]
+    b23 = T[661: 1020, k-1]
+    b24 = T[1021: M-1, k-1]
+    b2 = np.hstack((b21, [0], b22, [0], b23, [0], b24))
     b = b1 + b2
     T[1:M-1, k] = np.linalg.solve(A, b)
 
@@ -102,10 +115,9 @@ X, U = np.meshgrid(tspan, xspan)
 
 ax = plt.axes(projection = '3d')
 ax.plot_surface(U, X, T, linewidth = 0, cmap = plt.cm.coolwarm)
-ax.set_xticks([0, 3,6,9,12,15])
+ax.set_xticks([0, 0.003,0.006,0.009,0.012,0.015])
 ax.set_xlabel('$x$')
 ax.set_ylabel('$t$')
-ax.set_zlabel('$u$')
+ax.set_zlabel('$T$')
 plt.tight_layout()
 plt.show()
-
