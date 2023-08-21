5996607bb3ba7e14ee9eb1f45243fbdc33670430
liar017 <142229265+liar017@users.noreply.github.com>
2023-08-21 20:15:28 +0800

    ver-1 daca

--- Amodel/Ant_Colony_Algorithm/1xDACA.py
+++ Amodel/Ant_Colony_Algorithm/1xDACA.py
@@ -2,7 +2,8 @@ import numpy as np
 import random as rd
 
 def optimizefunc(x):
-    return abs(abs(x - 0.5) - 0.25)
+    # return (x - 0.396086) ** 2 / 100000
+    return abs(abs(abs(x - 0.5) - 0.25)-0.125)
 
 
 def bet(T, ant):
@@ -63,6 +64,45 @@ def encode(elite, p):
     return code
 
 
+def best_around(best, K):
+    """
+    @name:局部搜索最优路径的边缘路径
+    @params:
+    best: 最优路径阵列
+    K: 全局权值
+    @return: 返回一个包含t0,T1的增量矩阵
+    """
+    num = []
+    for i in range(0, len(best)):
+        str0 = ""
+        for j in range(0, i+1):
+            str0 += str(best[j])
+        num.append(int(str0))
+    nump = np.array(num) + 1
+    numn = np.array(num) - 1
+
+    best_t0 = np.zeros(10)
+    best_T1 = np.zeros((len(best)-1, 10, 10))
+
+    if nump[0] != 10:
+        best_t0[nump[0]] += K/4
+    if numn[0] != -1:
+        best_t0[numn[0]] += K/4
+
+
+    for i in range(1, len(nump)):
+        if nump[i] != 10 ** (i + 1):
+            n = nump[i] % 10
+            m = (nump[i] // 10) % 10
+            best_T1[(i-1, m, n)] += K/4
+        if nump[i] != -1:
+            n = numn[i] % 10
+            m = (numn[i] // 10) % 10
+            best_T1[(i-1, m, n)] += K/4
+    T = [best_t0, best_T1]
+    return T
+
+
 def DACA(optfunc):
     """
     @name: 十进制蚁群算法
@@ -71,8 +111,8 @@ def DACA(optfunc):
     """
     num_ant = 1000
     elitist_num = 50
-    loop_times = 10
-    p = 3    # precision
+    loop_times = 20
+    p = 6    # precision
 
     # 初始化信息素矩阵
     # 初始点到十分位的信息素
@@ -114,6 +154,7 @@ def DACA(optfunc):
         best = optvalue[0]
 
         code = encode(elite_x, p)
+        best_path = code[0]
 
         # K 权重系数    Q 幂底系数(Q<1)   J 比例系数
         K = 5;     Q = 0.8;    J = 1000
@@ -129,12 +170,35 @@ def DACA(optfunc):
             for x in range(0, elitist_num):
                 delta1[(i, code[x][i], code[x][i+1])] += K * Q ** (J * (optvalue[x] - best))
 
-        rho = 0.4 # 挥发系数
-        t0 = (1 - rho) * t0 + delta0
-        T1 = (1 - rho) * T1 + delta1
+        # 获得局部搜索边缘权值矩阵
+        T = best_around(best_path, K)
 
-    print(t0)
-    print(T1)
+        rho = 0.4 # 挥发系数
+        t0 = (1 - rho) * t0 + delta0 + T[0]
+        T1 = (1 - rho) * T1 + delta1 + T[1]
+
+
+    """
+    输出最终的信息素矩阵
+    """
+    # print(t0)
+    # print(T1)
+
+
+    # 输出大于epsilon的信息素转移项，构成最终转移路径矩阵
+    epsilon = 10
+    print("0->>1:", end='  ')
+    for i in range(0, len(t0)):
+        if(t0[i] > epsilon):
+            print("0->" + str(i), end=' ')
+    print("")
+    for i in range(0, len(T1)):
+        print(str(i+1) +"->>"+  str(i+2) + ":", end='  ')
+        for j in range(0, 10):
+            for k in range(0, 10):
+                if(T1[(i,j,k)] > epsilon):
+                    print(str(j) + "->" + str(k), end=' ')
+        print("")
 
 
 
